546f25fdfade5be178093798f19f79d80e1a6cb3
Inae Choi <inaechoi.dev@gmail.com>
2025-02-20 01:50:09 +0900

    feat: 게임 메시지 상수화

---
+++ src/constants/messages.ts
@@ -0,0 +1,17 @@
+const START_OR_END = "게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.\n";
+const END = "애플리케이션이 종료되었습니다.";
+const INPUT_ERROR = "\n잘못된 입력입니다. 다시 입력해주세요.\n";
+
+const GENERATE_NUMBER = "\n컴퓨터가 숫자를 뽑았습니다.\n";
+
+const INPUT_NUMBER = "숫자를 입력해주세요: ";
+const STRIKE = "3개의 숫자를 모두 맞히셨습니다.\n-------게임 종료-------";
+
+module.exports = {
+  START_OR_END,
+  END,
+  INPUT_ERROR,
+  GENERATE_NUMBER,
+  INPUT_NUMBER,
+  STRIKE,
+};
