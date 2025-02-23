1fe00b0fefbf03947a94211289a752d3abe25850
Inae Choi <inaechoi.dev@gmail.com>
2025-02-24 02:01:16 +0900

    feat: 게임 결과 출력하는 함수 구현

--- src/views/prompt.ts
+++ src/views/prompt.ts
@@ -1,5 +1,8 @@
 import { rl } from "../rl.js";
 import * as message from "../constants/messages.js";
+import { gameResults } from "../data.js";
+import { formatDateOptions } from "../util.js";
+import { GameResult } from "@/types/result.js";
 
 const getUserInput = (prompt: string): Promise<string> =>
   new Promise((resolve) => {
@@ -27,16 +30,57 @@ const printResult = (balls: number, strikes: number) => {
 
   if (!balls && !strikes) result = message.NOTHING;
 
-  if (balls || strikes)
+  if (balls || strikes) {
     result = [
       balls ? `${balls}${message.BALL}` : "",
       strikes ? `${strikes}${message.STRIKE}` : "",
     ]
       .join(" ")
       .trim();
+  }
 
   console.log(result);
   return { status: true, result };
 };
 
-export { getUserInput, showEndGame, printResult };
+const printLogs = async () => {
+  if (!gameResults.length) {
+    console.log(message.NO_LOGS);
+    return;
+  }
+
+  const resultList = gameResults.map(({ id, startTime, endTime, tryCount }) => [
+    `[${id}] / 시작시간: ${startTime?.toLocaleString(
+      "ko-KR",
+      formatDateOptions
+    )} / 종료시간: ${endTime?.toLocaleString(
+      "ko-KR",
+      formatDateOptions
+    )} / 횟수: ${tryCount}`,
+  ]);
+  console.log("게임 기록");
+  console.log(resultList.join("\n"));
+
+  const gameNumber = await getUserInput(
+    "\n확인할 게임 번호를 입력하세요 (종료하려면 0을 입력):"
+  );
+  if (gameNumber === "0") return;
+  if (+gameNumber > gameResults.length) {
+    console.log(message.NO_LOGS);
+    return;
+  }
+
+  const log: GameResult | undefined = gameResults.find(
+    ({ id }) => id === +gameNumber
+  );
+
+  console.log(
+    `\n${log?.id}번 게임 결과\n${log?.logs
+      ?.map(({ input, result }) => `숫자를 입력해주세요: ${input}\n${result}`)
+      .join(
+        "\n"
+      )}\n\n3개의 숫자를 모두 맞히셨습니다.\n-------기록 종료-------\n\n`
+  );
+};
+
+export { getUserInput, showEndGame, printResult, printLogs };
