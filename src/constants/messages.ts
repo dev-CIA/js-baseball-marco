89da7e47da5327f2667f1bf61b0ff3939a730c17
Inae Choi <inaechoi.dev@gmail.com>
2025-02-24 02:01:27 +0900

    feat: 메시지 상수 추가

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,5 +1,7 @@
-const START_OR_END = "게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.\n";
+const START_OR_END =
+  "게임을 새로 시작하려면 1, 기록을 보려면 2, 종료하려면 9을 입력하세요.\n";
 const END = "애플리케이션이 종료되었습니다.";
+const NO_LOGS = "\n저장된 게임 기록이 없습니다.\n";
 const INPUT_ERROR = "\n잘못된 입력입니다. 다시 입력해주세요.\n";
 const INPUT_ERROR_3_OTHERS_NUM =
   "1~9까지의 서로 다른 3자리 숫자를 입력해주세요.\n";
@@ -16,6 +18,7 @@ const STRIKE = "스트라이크";
 export {
   START_OR_END,
   END,
+  NO_LOGS,
   INPUT_ERROR,
   INPUT_ERROR_3_OTHERS_NUM,
   GENERATE_NUMBER,
